e27f9807811e6abbee357013677b2454de4e3e93
Michael Torres <michael.46@live.it>
2025-07-01 00:49:57 +0200

    Header components and folder structure created

--- sofiablackthorn-website/app.vue
+++ sofiablackthorn-website/app.vue
@@ -1,6 +1,44 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <section class="main-section">
+      <div class="header-container">
+        <Header />
+      </div>
+    </section>
   </div>
 </template>
+
+<script lang="ts" setup>
+import "./assets/font.css";
+import "./assets/color.css";
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  background: #1f1d1d;
+  background: linear-gradient(
+    90deg,
+    rgba(31, 29, 29, 1) 0%,
+    rgba(63, 27, 194, 1) 100%
+  );
+}
+
+.main-section {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.header-container {
+  max-width: 1200px;
+  margin: 0 auto;
+}
+</style>
---
+++ sofiablackthorn-website/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="header font-poppins-regular">
+    <a href="#">Social Media</a>
+    <a href="#">For Collaborations</a>
+    <span></span>
+    <a class="btn" href="#">Guarda Ora</a>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style>
+.header {
+  display: flex;
+  gap: 30px;
+  align-items: center;
+  padding: 10px 20px;
+}
+.header a {
+  color: var(--color-text);
+  text-decoration: none;
+  font-size: 1.2em;
+}
+.header a.btn {
+  background-color: var(--color-violet);
+  color: var(--color-text);
+  padding: 16px 20px;
+  border-radius: 5px;
+}
+</style>
